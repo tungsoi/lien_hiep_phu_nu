f212c6cbfda2f1d17dfa4ddc3c356e382db7ddb4
tungsoi <thanhtung.atptit@gmail.com>
2020-04-28 23:52:40 +0700

    add send email

--- app/Console/Commands/FetchPrizeOnWeek.php
+++ app/Console/Commands/FetchPrizeOnWeek.php
@@ -3,13 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Models\Answer;
-use App\Models\Member;
 use App\Models\MemberExam;
-use App\Models\Prize;
-use App\Models\Question;
 use App\Models\Week;
-use App\Models\WeekPrize;
+use Illuminate\Support\Facades\Mail;
 
 class FetchPrizeOnWeek extends Command
 {
@@ -45,69 +41,103 @@ class FetchPrizeOnWeek extends Command
     public function handle()
     {
         try {
-
-// Xử lý dữ liệu trả lời của tuần hiện tại
             // Tuần thi hiện tại
             $week = Week::where('status', 1)->first();
 
-            // Danh sách giải thưởng của tuần quay
-            $prizes = $week->prizes;
+            echo "-------------------\n";
+            echo "- Get current week \n";
+            if (!is_null($week) && $week->date_end < date('Y-m-d H:i:s', strtotime(now())))
+            {
+                echo "- Handing prizes... \n";
+                //  --> Xử lý dữ liệu trả lời của tuần hiện tại
+                // Danh sách giải thưởng của tuần quay
+                $prizes = $week->prizes;
 
-            // Danh sách tất cả các câu trả lời đúng (chưa check trùng người tham gia)
-            $exams = MemberExam::where('week_id', $week->id)
-                ->where('result', 1)
-                ->where('created_at', '<', $week->date_end)
-                ->get();
+                // Danh sách tất cả các câu trả lời đúng (chưa check trùng người tham gia)
+                $exams = MemberExam::where('week_id', $week->id)
+                    ->where('result', 1)
+                    ->where('created_at', '<', $week->date_end)
+                    ->get();
 
-            // Số lượng người tham gia có câu trả lời đúng
-            $number_people_corrects = $exams->groupBy('user_id')->count();
+                // Số lượng người tham gia có câu trả lời đúng
+                $number_people_corrects = $exams->groupBy('user_id')->count();
 
-            if ($exams) {
-                foreach ($exams as $exam) {
-                    // Tính hiệu số giữa dự đoán và số người trả lời đúng thực tế
-                    $sub = $exam->people_number > $number_people_corrects
-                        ? $exam->people_number - $number_people_corrects
-                        : - $exam->people_number + $number_people_corrects;
+                if ($exams) {
+                    foreach ($exams as $exam) {
+                        // Tính hiệu số giữa dự đoán và số người trả lời đúng thực tế
+                        $sub = $exam->people_number > $number_people_corrects
+                            ? $exam->people_number - $number_people_corrects
+                            : - $exam->people_number + $number_people_corrects;
 
-                    // Update vào database
-                    if ($exam->sub == "") {
-                        $exam->sub = $sub;
-                        $exam->save();
+                        // Update vào database
+                        if ($exam->sub == "") {
+                            $exam->sub = $sub;
+                            $exam->save();
+                        }
+                    }
+
+                    // Lấy list câu trả lời sau khi đã sắp xếp theo số dự đoán
+                    $list = MemberExam::where('week_id', $week->id)
+                            ->whereNotNull('sub')
+                            ->orderBy('sub', 'asc')
+                            ->orderBy('created_at', 'asc')
+                            ->get();
+
+                    // Lọc trùng 1 người trà lời
+                    $list_unique = $list->groupBy('user_id');
+
+                    // Lấy ra id cuối cùng
+                    $data_final = [];
+                    $flag = 0;
+                    foreach ($list_unique as $key => $row) {
+                        if ($flag < 11) {
+                            $data_final[] = $row[0]->id;
+                        }
+                        $flag++;
+                    }
+
+                    // Đẩy vào danh sách giải bảng week_prizes
+                    foreach ($prizes as $key => $prize) {
+                        if (isset($data_final[$key]) ) {
+                            $prize->member_exam_id = $data_final[$key];
+                            $prize->save();
+                        }
                     }
                 }
 
-                // Lấy list câu trả lời sau khi đã sắp xếp theo số dự đoán
-                $list = MemberExam::where('week_id', $week->id)
-                        ->whereNotNull('sub')
-                        ->orderBy('sub', 'asc')
-                        ->orderBy('created_at', 'asc')
-                        ->get();
+                echo "- Change status to next week \n";
+                //  --> Đổi trạng thái sang tuần thi khác
+                $week->status = 2;
+                $week->save();
 
-                // Lọc trùng 1 người trà lời
-                $list_unique = $list->groupBy('user_id');
-
-                // Lấy ra id cuối cùng
-                $data_final = [];
-                $flag = 0;
-                foreach ($list_unique as $key => $row) {
-                    if ($flag < 8) {
-                        $data_final[] = $row[0]->id;
-                    }
-                    $flag++;
+                $nextWeek = Week::where('status', 0)->where('date_start', '>', $week->date_end)->orderBy('date_start', 'asc')->first();
+                if (!is_null($nextWeek)) {
+                    $nextWeek->status = 1;
+                    $nextWeek->save();
                 }
 
-                // Đẩy vào danh sách giải bảng week_prizes
-                foreach ($prizes as $key => $prize) {
-                    if (isset($data_final[$key]) ) {
-                        $prize->member_exam_id = $data_final[$key];
-                        $prize->save();
-                    }
+                //  --> Gửi email thông báo cho người trúng thưởng
+                foreach($data_final as $email_key => $member_exam_id) {
+                    // $member_email = MemberExam::find($member_exam_id)->member->email; live
+                    $member_email = 'thanhtung.atptit@gmail.com'; // test
+                    $email = [
+                        'view'      =>  'member.email',
+                        'content'   =>  [
+                            'Content'   =>  '',
+                            'Header'    =>  'Thông báo trúng thưởng giải tuần Hội liên hiệp Phụ nữ Việt Nam'
+                        ],
+                        'subject'   =>  'Thông báo trúng thưởng giải tuần Hội liên hiệp Phụ nữ Việt Nam'
+                    ];
+
+                    echo "- $email_key - Sending email to $member_email \n";
+                    Mail::send($email['view'], [
+                        'member_email' => $member_email,
+                        'emailContent' => $email['content']
+                    ], function($m) use ($member_email, $email) {
+                        $m->to($member_email, 'Khách hàng tham gia dự thi')->subject($email['subject']);
+                    });
                 }
             }
-
-// Đổi trạng thái sang tuần thi khác
-
-            echo "- success\n";
             return true;
         } catch (\Exception $e) {
             dd($e);
---
+++ resources/views/member/email.blade.php
@@ -0,0 +1,11 @@
+Hội Liên hiệp Phụ nữ Việt Nam <br/>
+<br/>
+THÔNG BÁO<br/>
+<br/>
+Chúc mừng bạn đã đạt Giải thưởng Tuần của Cuộc thi trắc nghiệm<br/>
+"Tìm hiểu 90 năm lịch sử hình thành và phát triển Hội Liên hiệp Phụ nữ Việt Nam"<br/>
+<br/>
+Để nhận giải thưởng, bạn có thể tới trực tiếp địa chỉ: Ban Tuyên giáo Trung ương Hội Liên Hiệp Phụ nữ Việt Nam, 68, Nguyễn Chí Thanh, Hà Nội. <br/>Hoặc nhận giải thưởng theo hình thức chuyển khoản.<br/>
+<br/>Chi tiết xin liên hệ với Đ/c Phạm Bình Minh, chuyên viên Ban Tuyên giáo, TW Hội Liên hiệp Phụ nữ Việt Nam. <br/><br/>Số điện thoại: 0243.9720041/ 0962.374.676<br/>
+Email: minhphamussh@gmail.com<br/><br/>
+Xin trân trọng cảm ơn!<br/>
